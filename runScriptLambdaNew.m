39ae1870a6ab27fbae27d7b28128207202b07e58
Quico Spaen <quico@spaen.nl>
2016-02-02 18:26:53 -0800

    Add compiling mex file to example

--- runScriptLambdaNew.m
+++ runScriptLambdaNew.m
@@ -1,3 +1,6 @@
+% compile mex file for running c Library in Matlab
+mex -largeArrayDims hpf.c
+
 lowProblem = CutProblem( 8, 1, 8 , capLabels, cap, sourceWeights, sinkWeights, lambdaMultSource, lambdaMultSink, 0, 0, lambdaLow );
 highProblem = CutProblem( 8, 1, 8 , capLabels, cap, sourceWeights, sinkWeights, lambdaMultSource, lambdaMultSink, 0, 0, lambdaHigh );
 
