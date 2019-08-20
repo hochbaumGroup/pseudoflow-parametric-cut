2b323ddf910b615a5b133b80cd56e852690a3f58
Quico Spaen <quico@spaen.nl>
2019-08-20 15:58:43 -0700

    bump version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ extensions = [
 
 setup(
     name="pseudoflow",
-    version="2019.8.2",
+    version="2019.8.3",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
