6621f83339a0bff407e697b8a285e6a858e0d415
Quico Spaen <quico@spaen.nl>
2022-12-31 16:57:59 -0800

    version bump for igraph support

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ extensions = [
 
 setup(
     name="pseudoflow",
-    version="2020.6.1",
+    version="2022.12.0",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
